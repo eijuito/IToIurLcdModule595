c1c2ff1aa3e48de74e19cd0db9b5afaefda2e230
Celso Eiju Ito <eijuito@gmail.com>
2016-08-08 00:07:03 -0300

    20160808 0006 First commit

---
+++ IToIurLcdModule595.cpp
@@ -0,0 +1,18 @@
+/*
+ * IToIurLcdModule595.cpp
+ *
+ *  Created on: Aug 8, 2016
+ *      Author: Administrator
+ */
+
+#include "IToIurLcdModule595.h"
+
+IToIurLcdModule595::IToIurLcdModule595() {
+	// TODO Auto-generated constructor stub
+
+}
+
+IToIurLcdModule595::~IToIurLcdModule595() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ IToIurLcdModule595.h
@@ -0,0 +1,17 @@
+/*
+ * IToIurLcdModule595.h
+ *
+ *  Created on: Aug 8, 2016
+ *      Author: Administrator
+ */
+
+#ifndef ITOIURLCDMODULE595_H_
+#define ITOIURLCDMODULE595_H_
+
+class IToIurLcdModule595 {
+public:
+	IToIurLcdModule595();
+	virtual ~IToIurLcdModule595();
+};
+
+#endif /* ITOIURLCDMODULE595_H_ */
